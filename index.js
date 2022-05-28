11a262405928f4664d32a7fff1b6cf5c67acfda5
Robert Monfera <monfera.robert@gmail.com>
2022-05-28 02:09:04 +0200

    more idiomatic wgsl

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const code = /* language=WGSL */ `
-  struct Output {
+  struct VSOutput {
     @builtin(position) position: vec4<f32>,
     @location(0) color: vec4<f32>
   };
-  
+
+  let positions = array<vec2<f32>,3>(
+    vec2<f32>( 0.0,  0.5), 
+    vec2<f32>(-0.5, -0.5), 
+    vec2<f32>( 0.5, -0.5)
+  );
+
+  let colors = array<vec3<f32>,3>(
+    vec3<f32>(0.0, 1.0, 1.0), 
+    vec3<f32>(0.0, 0.0, 1.0), 
+    vec3<f32>(1.0, 0.0, 1.0)
+  );
+
   @stage(vertex)
-  fn vs_main(@builtin(vertex_index) vertexIndex: u32) -> Output {
-    var positions = array<vec2<f32>,3>(
-      vec2<f32>(0.0, 0.5), 
-      vec2<f32>(-0.5, -0.5), 
-      vec2<f32>(0.5, -0.5)
-    );
-    var colors = array<vec3<f32>,3>(
-      vec3<f32>(0.0, 1.0, 1.0), 
-      vec3<f32>(0.0, 0.0, 1.0), 
-      vec3<f32>(1.0, 0.0, 1.0)
-    );
-    return Output(
+  fn vs_main(@builtin(vertex_index) vertexIndex: u32) -> VSOutput {
+    return VSOutput(
       vec4<f32>(positions[vertexIndex], 0.0, 1.0),
       vec4<f32>(colors[vertexIndex], 1.0)
     );
   }
-  
+
   @stage(fragment)
   fn fs_main(@location(0) color: vec4<f32>) -> @location(0) vec4<f32> {
     return color;
